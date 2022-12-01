210a1b199f7cc476cdd88be6745c0e6e43ea97c2
azharjk <azhr.project@gmail.com>
2022-12-01 10:28:19 +0700

    Web: Add primary variant header
    This header is used for login page.

---
+++ apps/Web/components/PrimaryHeader.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { formatDate, formatTime } from '../utils';
+
+export default function PrimaryHeader() {
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const d = new Date();
+      setDate(formatDate(d));
+      setTime(formatTime(d));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <header className="primary-header">
+      <div>
+        <Image
+          src="/itenas-logo.png"
+          alt="Itenas logo"
+          width={170}
+          height={56}
+        />
+      </div>
+      <div className="timespan">
+        <span>{date}</span>
+        {` `}
+        <span>{time}</span>
+      </div>
+    </header>
+  );
+}
