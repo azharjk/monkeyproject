21009c0be992d20afb493bafece9220dcf7d41a0
azharjk <azhr.project@gmail.com>
2022-12-08 17:13:48 +0700

    Web: Change title on home page

--- apps/Web/pages/index.tsx
+++ apps/Web/pages/index.tsx
@@ -1,8 +1,13 @@
+import Head from 'next/head';
 import Navbar from '../components/Navbar';
+import Project from '../meta/project';
 
 export default function RootPage() {
   return (
     <>
+      <Head>
+        <title>{`Home | ${Project.Name}`}</title>
+      </Head>
       <Navbar />
       <h1>Hello, world</h1>
     </>
