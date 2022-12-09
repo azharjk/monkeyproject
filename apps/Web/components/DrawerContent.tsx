016464679ceb4a4d39b83e3995d07db86bf498d9
azharjk <azhr.project@gmail.com>
2022-12-09 19:25:49 +0700

    Web: Add DrawerContent
    This patch get rid of the overflow-y hidden problem, because in the end the content
    it self is gonna overflow to the y direction. So why we add overflow-y: hidden at first.

--- apps/Web/components/DrawerContent.tsx
+++ apps/Web/components/DrawerContent.tsx
@@ -2,7 +2,7 @@ export default function DrawerContent() {
   return (
     <ul className="drawer-content-list">
       <li className="drawer-content-list__item">
-        <span className="drawer-content-list__item-title">Inquiry</span>
+        <span className="bold drawer-content-list__item-title">Inquiry</span>
         <ul className="drawer-content-subcontent-list">
           <li>Data mahasiswa</li>
           <li>Nilai semester</li>
@@ -13,8 +13,25 @@ export default function DrawerContent() {
           <li>Hasil KRS dan Tagihan awal</li>
         </ul>
       </li>
-      <li className="drawer-content-list__item">Pendaftaran semester</li>
-      <li className="drawer-content-list__item">Lain - lain</li>
+      <li className="drawer-content-list__item">
+        <span className="bold drawer-content-list__item-title drawer-content-list__item-title--not-first">
+          Pendaftaran Semester
+        </span>
+        <ul className="drawer-content-subcontent-list">
+          <li>Jadwal kuliah perwalian</li>
+          <li>Perwalian</li>
+          <li>Batal tambah (BDATM)</li>
+        </ul>
+      </li>
+      <li className="drawer-content-list__item">
+        <span className="bold drawer-content-list__item-title drawer-content-list__item-title--not-first">
+          Lain - lain
+        </span>
+        <ul className="drawer-content-subcontent-list">
+          <li>Permohonan angsuran</li>
+          <li>Cetak lebih bayar</li>
+        </ul>
+      </li>
     </ul>
   );
 }
