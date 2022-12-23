50fd7c977a0ef60f8b31aed611da8b71dbe6548a
azharjk <azhr.project@gmail.com>
2022-12-23 16:15:31 +0700

    Api: Seed aktivitas kuliah on startup

--- apps/Api/src/main/java/com/github/azharjk/monkeyproject/api/OnStartup.java
+++ apps/Api/src/main/java/com/github/azharjk/monkeyproject/api/OnStartup.java
@@ -1,9 +1,13 @@
 package com.github.azharjk.monkeyproject.api;
 
+import com.github.azharjk.monkeyproject.api.AktivitasKuliah.AktivitasKuliah;
+import com.github.azharjk.monkeyproject.api.AktivitasKuliah.AktivitasKuliahRepository;
 import com.github.azharjk.monkeyproject.api.mahasiswa.Mahasiswa;
 import com.github.azharjk.monkeyproject.api.mahasiswa.MahasiswaRepository;
 import com.github.azharjk.monkeyproject.api.matakuliah.MataKuliah;
 import com.github.azharjk.monkeyproject.api.matakuliah.MataKuliahRepository;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,13 +21,16 @@ public class OnStartup implements ApplicationRunner {
     
     private final String MAKE_DEFAULT_USER = "make.default_user";
     private final String SEED_MATA_KULIAH = "seed.mata_kuliah";
+    private final String SEED_AKTIVITAS_KULIAH = "seed.aktivitas_kuliah";
     
     private final MataKuliahRepository mataKuliahRepository;
     private final MahasiswaRepository mahasiswaRepository;
+    private final AktivitasKuliahRepository aktivitasKuliahRepository;
 
-    public OnStartup(MataKuliahRepository mataKuliahRepository, MahasiswaRepository mahasiswaRepository) {
+    public OnStartup(MataKuliahRepository mataKuliahRepository, MahasiswaRepository mahasiswaRepository, AktivitasKuliahRepository aktivitasKuliahRepository) {
         this.mataKuliahRepository = mataKuliahRepository;
         this.mahasiswaRepository = mahasiswaRepository;
+        this.aktivitasKuliahRepository = aktivitasKuliahRepository;
     }
 
     @Override
@@ -40,6 +47,20 @@ public class OnStartup implements ApplicationRunner {
             mataKuliahRepository.save(new MataKuliah("Matematika", 3));
             mataKuliahRepository.save(new MataKuliah("Matematika Komputer", 3));
             mataKuliahRepository.save(new MataKuliah("Algoritma Dasar", 3));
+        }
+        
+        if (optionNames.contains(SEED_AKTIVITAS_KULIAH)) {
+            if (optionNames.contains(MAKE_DEFAULT_USER) && optionNames.contains(SEED_MATA_KULIAH)) {
+                logger.info("Seed aktivitas kuliah");
+                
+                Mahasiswa mahasiswa = mahasiswaRepository.findById(1L).orElseThrow();
+                MataKuliah mataKuliah = mataKuliahRepository.findById(1L).orElseThrow();
+                aktivitasKuliahRepository.save(new AktivitasKuliah(LocalDate.now(), LocalTime.now(), mataKuliah, mahasiswa));
+                
+                mataKuliah = mataKuliahRepository.findById(2L).orElseThrow();
+                aktivitasKuliahRepository.save(new AktivitasKuliah(LocalDate.now().plusDays(2), LocalTime.now(), mataKuliah, mahasiswa));
+
+            }
         }
     }
     
