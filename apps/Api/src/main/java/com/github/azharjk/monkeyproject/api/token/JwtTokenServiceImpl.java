a833fe8e710b692847ac20b5cb264cbed707182a
azharjk <azhr.project@gmail.com>
2022-12-24 16:39:58 +0700

    Api: Make RSAKey bean able

--- apps/Api/src/main/java/com/github/azharjk/monkeyproject/api/token/JwtTokenServiceImpl.java
+++ apps/Api/src/main/java/com/github/azharjk/monkeyproject/api/token/JwtTokenServiceImpl.java
@@ -3,20 +3,19 @@ package com.github.azharjk.monkeyproject.api.token;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.github.azharjk.monkeyproject.api.mahasiswa.Mahasiswa;
-import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class JwtTokenServiceImpl implements TokenService {
-    
-    private static final Logger logger = LoggerFactory.getLogger(JwtTokenServiceImpl.class);
+        
+    private final RSAKey rsaKey;
+
+    public JwtTokenServiceImpl(RSAKey rsaKey) {
+        this.rsaKey = rsaKey;
+    }
 
     @Override
     public TokenPair generate(Mahasiswa mahasiswa) {
-        try {
-            RSAKey rsaKey = RSAKey.create();
             Algorithm algorithm = Algorithm.RSA256(rsaKey.getPublicKey(), rsaKey.getPrivateKey());
             
             String accessToken = JWT.create()
@@ -32,9 +31,6 @@ public class JwtTokenServiceImpl implements TokenService {
                     .sign(algorithm);
             
             return new TokenPair(accessToken, refreshToken);
-        } catch (NoSuchAlgorithmException ex) {
-            logger.error("Exception occured when creating JWT token, details: ", ex);
-        }
-        return null;
+        
     }
 }
--- apps/Api/src/main/java/com/github/azharjk/monkeyproject/api/token/TokenConfiguration.java
+++ apps/Api/src/main/java/com/github/azharjk/monkeyproject/api/token/TokenConfiguration.java
@@ -6,7 +6,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TokenConfiguration {
     @Bean
-    public TokenService tokenService() {
-        return new JwtTokenServiceImpl();
+    public TokenService tokenService() throws Exception {
+        return new JwtTokenServiceImpl(rsaKey());
+    }
+    
+    @Bean
+    public RSAKey rsaKey() throws Exception {
+        return RSAKey.create();
     }
 }
